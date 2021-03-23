cb27bf5b4dfe0d10a26b0c112e91504a8595dd9c
st0llerhub <st0ller@outlook.com>
2021-03-23 11:43:20 +0100

    Add initial Jenkinsfile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,6 +15,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getABC());
     }
 
     private final String getMessage() {
