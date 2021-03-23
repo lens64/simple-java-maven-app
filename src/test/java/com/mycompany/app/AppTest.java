ae18659e84e56234ca7d4dc6bc293d9b2f996211
st0llerhub <st0ller@outlook.com>
2021-03-23 11:46:34 +0100

    Added second test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -35,6 +35,7 @@ public class AppTest
         App.main(null);
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hallo Welt!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
